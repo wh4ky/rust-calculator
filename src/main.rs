a1b070c921ab30e7698576d876e465efb18479fa
wh4ky <damian.whacky@proton.me>
2023-12-25 11:32:39 +0100

    basic addition

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
+use std::io;
+
 fn main() {
-    println!("Enter a few values.");
-    
+    let mut value = String::new().to_string();
+    println!("Enter the first value.");
+    io::stdin().read_line(&mut value).expect("error");
+    let value_1: u32 = value.trim().parse::<u32>().unwrap();
+
+    value = String::new().to_string();
+
+    println!("Enter the second value.");
+    io::stdin().read_line(&mut value).expect("error");
+    let value_2: u32 = value.trim().parse::<u32>().unwrap();
+
+    let sum: u32 = value_1 + value_2;
+
+    println!("The sum is = {} + {} = {}", value_1, value_2, sum);
 }
